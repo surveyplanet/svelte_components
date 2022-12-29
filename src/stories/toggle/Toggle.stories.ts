57fa9dbf5b2d40bb4e6cfd82ea05064389c8362f
serferdinand2 <ognjen@surveyplanet.com>
2022-12-29 18:24:51 +0100

    added badges

---
+++ src/stories/toggle/Toggle.stories.ts
@@ -0,0 +1,24 @@
+// import type { PlayFunctionContext } from '@storybook/types';
+import type { Meta, StoryObj } from '@storybook/svelte';
+import Toggle from '../../lib/Toggle.svelte';
+import { within, userEvent } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+// More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction#default-export
+const meta: Meta<Toggle> = {
+	title: 'Toggle/Default',
+	component: Toggle,
+};
+
+export default meta;
+type Story = StoryObj<Toggle>;
+
+export const Default: Story = {
+	args: {},
+	render: (args: StoryObj) => {
+		return {
+			Component: Toggle,
+			props: args,
+		};
+	},
+};
