0d6e8741d6bc998bc281fb9be3dd866c93100dc3
Jonah Werre <jonahwerre@gmail.com>
2023-01-03 12:10:30 -0700

    removed icons directory

---
+++ src/lib/_definitions.ts
@@ -0,0 +1,13 @@
+export const enum BUTTON_SIZES {
+	SMALL = 'small',
+	MEDIUM = 'medium',
+	LARGE = 'large',
+}
+
+export const enum BUTTON_MODES {
+	PRIMARY = 'primary',
+	SECONDARY = 'secondary',
+	TERTIARY = 'tertiary',
+	QUATERNARY = 'quaternary',
+	DARK = 'dark',
+}
