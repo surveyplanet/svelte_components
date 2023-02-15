e5087dc5f8d8d4cda315b0d99e6ae51e8c01ad7a
serferdinand2 <ognjen@surveyplanet.com>
2023-02-15 17:24:06 +0100

    added form folder in the srttories

---
+++ src/lib/Form.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let id: string = (Date.now() + Math.random()).toString(36);
+	export let trigger = '';
+
+	const submitEventHandler = (event: Event) => {
+		console.log('submitEventHandler');
+	};
+</script>
+
+<form
+	class="sp-form"
+	{id}>
+	<slot />
+</form>
---
+++ src/stories/form/Form.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+	import { TextInput } from '../../lib';
+	import * as test from './form.test';
+</script>
+
+<Meta
+	title="Form controls/Text Input"
+	component={TextInput}
+	argTypes={{
+		id: { control: 'text' },
+		trigger: { control: 'text' },
+	}} />
+
+<Template let:args>
+	<TextInput {...args} />
+</Template>
+
+<Story name="Basic" />
