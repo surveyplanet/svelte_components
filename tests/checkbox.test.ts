c194eeb78095213e5bf9e78e9a28543bf41d0b09
serferdinand2 <ognjen@surveyplanet.com>
2023-03-02 19:01:03 +0100

    checkbox tests

--- tests/checkbox.test.ts
+++ tests/checkbox.test.ts
@@ -1,13 +1,13 @@
 import { test, expect } from '@playwright/test';
-import { loadStory, setControl } from './_utils.js';
+import { loadStory, setControl, getLastEvent } from './_utils.js';
 
 test.describe('Checkbox component', () => {
-	test('should render basic checkbox component', async ({ page }) => {
+	test.only('should render basic checkbox component', async ({ page }) => {
 		const preview = await loadStory(page, 'checkbox');
-		const value = 'Checkbox';
 		const label = preview.locator('.sp-checkbox');
-		const checkbox = preview.locator('#checkbox');
 		const labelText = label.locator('.sp-checkbox--label');
+
+		// const id = await checkbox.getAttribute('id');
 
 		await setControl(page, 'Name', 'text', 'checkbox');
 		await setControl(page, 'Label', 'text', 'Subscribe');
@@ -17,18 +17,19 @@ test.describe('Checkbox component', () => {
 		await expect(label).toBeVisible();
 
 		await label.click();
+		// const event = await getLastEvent(page);
+		// expect(event.name).toBe('change');
 
 		await expect(label).toBeChecked();
 
 		expect(label).toBeDefined();
 		await expect(label).toHaveClass(/sp-checkbox/);
 		await expect(label).not.toHaveClass(/sp-checkbox--prepend/);
-		expect(label).toHaveAttribute('for', checkbox.id);
+		await expect(label).toHaveAttribute('for', `checkbox`);
 
 		expect(labelText).toBeDefined();
 		await expect(labelText).toContainText('Subscribe');
 	});
-
 	test('should render checkbox and disabled it', async ({ page }) => {
 		const preview = await loadStory(page, 'checkbox');
 		const label = preview.locator('.sp-checkbox');
@@ -40,18 +41,20 @@ test.describe('Checkbox component', () => {
 		await expect(label).toBeDisabled();
 	});
 
-	// test('should render checkbox and prepend label', async ({ page }) => {
-	// 	const preview = await loadStory(page, 'checkbox');
-	// 	const label = preview.locator('.sp-checkbox');
+	test('should render checkbox and prepend label', async ({ page }) => {
+		const preview = await loadStory(page, 'checkbox');
+		const label = preview.locator('.sp-checkbox');
 
-	// 	await setControl(page, 'Prepend', 'checkbox', 'true');
+		await setControl(page, 'Prepend label', 'checkbox', 'true');
 
-	// 	await expect(label).toBeVisible();
-	// 	const labelStyles: CSSStyleDeclaration = window.getComputedStyle(label);
-	// 	expect(label).toBeDefined();
-	// 	expect(label).toHaveClass('sp-checkbox--prepend');
-	// 	expect(labelStyles.getPropertyValue('flex-direction')).toBe(
-	// 		'row-reverse'
-	// 	);
-	// });
+		await expect(label).toBeVisible();
+		const labelStyles = await label.evaluate((el) => {
+			return window
+				.getComputedStyle(el)
+				.getPropertyValue('flex-direction');
+		});
+		expect(label).toBeDefined();
+		await expect(label).toHaveClass(/sp-checkbox--prepend/);
+		expect(labelStyles).toBe('row-reverse');
+	});
 });
