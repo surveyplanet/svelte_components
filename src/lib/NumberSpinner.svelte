04c84c64100415d214cf0b6086cd652e7f13d1cd
serferdinand2 <ognjen@surveyplanet.com>
2023-03-29 18:51:27 +0200

    added time

--- src/lib/NumberSpinner.svelte
+++ src/lib/NumberSpinner.svelte
@@ -2,31 +2,45 @@
 	import { createEventDispatcher } from 'svelte';
 	import Icon from './Icon.svelte';
 
+	export let label = '';
 	export let step = 1;
 	export let min = 0;
 	export let max = 100;
 	export let value = 0;
 
+	export let disabled = false;
+	export let required = false;
+	//overflow is used to handle overflow of the input value and goes from max to min and vice versa
+	export let overflow = false;
+	export let time = false;
+	export let currency = false;
+	export let id = '';
+
 	const dispatch: (name: string, detail: number) => boolean =
 		createEventDispatcher();
 
 	const increment = () => {
+		// let currentVal;
 		if (value + step <= max) {
 			value += step;
-			dispatch('change', value);
+			if (overflow && value + step > max) value = min;
+		} else {
+			value = max;
 		}
+		dispatch('change', value);
 	};
 
 	const decrement = () => {
 		if (value - step >= min) {
 			value -= step;
-			dispatch('change', value);
+			if (overflow && value - step < min) value = max;
 		} else {
 			value = min;
-			dispatch('change', value);
-			console.log('value is too small');
 		}
+		dispatch('change', value);
 	};
+
+	// use parseFloat to handle decimal numbers
 
 	const inputChange = (e: Event) => {
 		const input = e.target as HTMLInputElement;
@@ -35,14 +49,42 @@
 		if (val >= min && val <= max) {
 			value = val;
 			dispatch('change', value);
+		}
+	};
+
+	const setTwoNumberDecimal = (value) => {
+		if (time) {
+			const input = value.target as HTMLInputElement;
+			const val = input.value;
+			const timeRegex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+			if (timeRegex.test(val)) {
+				input.value = val;
+			} else {
+				input.value = '';
+			}
+		} else if (currency) {
+			const input = value.target as HTMLInputElement;
+			const val = input.value;
+			const currencyRegex = /^\d+(?:\.\d{0,2})$/;
+			if (currencyRegex.test(val)) {
+				input.value = val;
+			} else {
+				input.value = '';
+			}
 		} else {
-			value = max;
-			dispatch('change', value);
-			console.log('value is too big');
+			const input = value.target as HTMLInputElement;
+			const val = input.value;
+			const numberRegex = /^\d+$/;
+			if (numberRegex.test(val)) {
+				input.value = val;
+			} else {
+				input.value = '';
+			}
 		}
 	};
 
 	const inputHandler = (event: Event) => {
+		setTwoNumberDecimal(event);
 		inputChange(event);
 	};
 
@@ -55,67 +97,119 @@
 	};
 </script>
 
+<label
+	for="sp-number-spinner"
+	class="sp-number-spinner--label">{label}</label>
 <div
-	class="number-spinner"
+	class="sp-number-spinner"
 	on:keydown={keydownHandler}>
 	<input
 		type="number"
 		bind:value
+		{step}
+		{id}
+		{disabled}
 		on:input={inputHandler} />
-	<button on:click={increment}>
-		<Icon name="chevronUp" />
-	</button>
-	<button on:click={decrement}>
-		<Icon name="chevronDown" />
-	</button>
+	<div class="sp-number-spinner--buttons">
+		<button
+			class="sp-number-spinner--button"
+			on:click={increment}>
+			<svg
+				class="sp-dropdown--toggle-icon"
+				width="7"
+				height="4"
+				viewBox="0 0 7 4"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg">
+				<path
+					d="M1.08984 0.830868L3.50606 3.24707L6.0002 0.75293"
+					stroke="#162137"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+			</svg>
+		</button>
+		<button
+			class="sp-number-spinner--button"
+			on:click={decrement}>
+			<svg
+				class="sp-dropdown--toggle-icon"
+				width="7"
+				height="4"
+				viewBox="0 0 7 4"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg">
+				<path
+					d="M1.08984 0.830868L3.50606 3.24707L6.0002 0.75293"
+					stroke="#162137"
+					stroke-width="1.5"
+					stroke-linecap="round"
+					stroke-linejoin="round" />
+			</svg>
+		</button>
+	</div>
 </div>
 
 <style lang="scss">
 	@use '@surveyplanet/styles' as *;
 
-	.number-spinner {
+	.sp-number-spinner {
 		display: flex;
 		align-items: center;
-		justify-content: center;
-		border: 1px solid;
+		justify-content: space-between;
+		width: 100%;
+		height: $size-gutter;
+		border: 1px solid $color--slate;
 		border-radius: 4px;
-		width: 100px;
-		height: 32px;
-		font-size: 16px;
-		line-height: 24px;
+		padding: 0 8px;
+		color: red;
 
-		button {
-			width: 32px;
-			height: 32px;
+		&:focus-within {
+			border-color: $color--light-purple-dark;
+		}
+
+		&--label {
+			display: block;
+			margin-bottom: 4px;
+			font-size: 14px;
+			line-height: 20px;
+			color: $color--slate;
+		}
+
+		&--buttons {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			height: 100%;
+		}
+
+		&--button {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 24px;
+			height: 24px;
 			border: none;
 			border-radius: 4px;
-
-			cursor: pointer;
-			outline: none;
+			background-color: $color--slate-lighter;
+			transition: background-color 0.2s ease-in-out;
 
 			&:hover {
-				background-color: $color--light-purple-light;
+				background-color: $color--light-purple-dark;
 			}
 
 			&:active {
-				background-color: $color--light-purple-light;
+				background-color: $color--light-purple-dark;
 			}
 		}
-
-		input {
-			width: 100%;
-			height: 100%;
-			border: none;
-			border-radius: 4px;
-
-			text-align: center;
-			outline: none;
-		}
-
-		input::-webkit-outer-spin-button,
-		input::-webkit-inner-spin-button {
-			-webkit-appearance: none;
-			margin: 0;
-		}
 	}
+
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+	// input[type='number'] {
+	// 	-moz-appearance: textfield;
+	// }
 </style>
--- src/stories/numberspinner/NumberSpinner.story.svelte
+++ src/stories/numberspinner/NumberSpinner.story.svelte
@@ -5,27 +5,67 @@
 	import { NumberSpinner } from '../../lib';
 	export let Hst: Histoire;
 
+	let label = 'Number Spinner';
 	let step = 1;
 	let min = 0;
 	let max = 100;
 	let value = 0;
+	let disabled = false;
+	let required = false;
+	let overflow = false;
+	let time = false;
+	let currency = false;
+	let id = '';
 </script>
 
 <Hst.Story title="Form controls / NumberSpinner">
 	<svelte:fragment slot="controls">
-		<!-- <Hst.Number
-			label="Value"
-			{value}
-			min={0}
-			max={100}
-			step={1} /> -->
+		<Hst.Text
+			bind:value={label}
+			title="Label" />
+		<Hst.Text
+			bind:value={id}
+			title="Id" />
+		<Hst.Number
+			bind:value={step}
+			title="Step" />
+		<Hst.Number
+			bind:value={min}
+			title="Min" />
+		<Hst.Number
+			bind:value={max}
+			title="Max" />
+		<Hst.Number
+			bind:value
+			title="Value" />
+		<Hst.Checkbox
+			bind:value={disabled}
+			title="Disabled" />
+		<Hst.Checkbox
+			bind:value={required}
+			title="Required" />
+		<Hst.Checkbox
+			bind:value={overflow}
+			title="Overflow" />
+		<Hst.Checkbox
+			bind:value={time}
+			title="Time" />
+		<Hst.Checkbox
+			bind:value={currency}
+			title="Currency" />
 	</svelte:fragment>
 
 	<Hst.Variant title="Primary">
 		<NumberSpinner
+			{label}
 			{step}
 			{min}
 			{max}
-			{value} />
+			{value}
+			{disabled}
+			{required}
+			{overflow}
+			{time}
+			{currency} />
 	</Hst.Variant>
 </Hst.Story>
