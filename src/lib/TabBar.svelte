67065f1bcd6d9f9e859d870f566130de1db4434a
serferdinand2 <ognjen@surveyplanet.com>
2023-04-05 19:30:22 +0200

    started the tab bar

---
+++ src/lib/TabBar.svelte
@@ -0,0 +1,65 @@
+<script lang="ts"
+	context="module">
+	import { Icon, type IconName } from './index';
+	export interface tabBarData {
+		id: string;
+		label?: string;
+		html?: string;
+		icon?: IconName;
+		selected?: boolean;
+	}
+
+</script>
+
+
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import { each } from 'svelte/internal';
+    import Icon from './Icon.svelte';
+
+
+	const dispatch: (name: string, index: number) => boolean =
+		createEventDispatcher();
+
+	/**
+	 * Tab bar id
+	 */
+
+	export let id: string = (Date.now() + Math.random()).toString(36);
+	/**
+	 * Index position of the active tab
+	 */
+	export let activeIndex = 0;
+	/**
+	 * Force tab to consume all available space
+	 */
+	export let grow = false;
+	/**
+	 * A set of indexes (starting at 0) corresponding to tab buttons to disable.
+	 */
+	export let disabledIndexes: number[] = [];
+
+	/**
+	 * Objects to pass to the tab buttons
+	 */
+	export let data: tabBarData[] = []
+
+
+</script>
+
+<ul
+	class="sp-tab-bar"
+	{id}>
+    {#each buttons as button, index}
+
+</ul>
+
+<style lang="scss">
+	@use '@surveyplanet/styles' as *;
+
+	.sp-tab-bar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
---
+++ src/stories/tabbar/TabBar.story.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { Hst as Histoire } from '@histoire/plugin-svelte';
+	import { json } from '@sveltejs/kit';
+	import { logEvent } from 'histoire/client';
+	import { TabBar, Button } from '../../lib';
+	// import source from './source';
+
+	export let Hst: Histoire;
+
+	let id = 'tab-bar';
+	let grow = false;
+	let disabledIndexes = [1];
+
+	const changeEventHandler = (e: Event): void => {
+		logEvent('change', e);
+	};
+</script>
+
+<Hst.Story title=" TabBar">
+	<svelte:fragment slot="controls">
+		<Hst.Text
+			bind:value={id}
+			title="Id" />
+		<Hst.Checkbox
+			bind:value={grow}
+			title="Grow" />
+		<Hst.Json
+			bind:value={disabledIndexes}
+			title="Disabled indexes" />
+	</svelte:fragment>
+
+	<Hst.Variant title="Primary">
+		<div id="wrapper">
+			<TabBar
+				{id}
+				{grow}
+				{disabledIndexes} />
+		</div>
+	</Hst.Variant>
+</Hst.Story>
