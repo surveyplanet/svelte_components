e801f980539703b4eac76fc2045245b2f6ca360b
serferdinand2 <ognjen@surveyplanet.com>
2023-04-12 21:59:13 +0200

    checkin, first itteration of navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,3 +1,50 @@
-<script context="module">
-	import { Icon, type IconData } from './Index';
+<script
+	context="module"
+	lang="ts">
+	import { Icon, type IconName } from './index';
+
+	export type NavBarData = {
+		icon: IconName;
+		title?: string;
+		link: string;
+	};
 </script>
+
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch: (name: string, detail: string) => boolean =
+		createEventDispatcher();
+
+	export let data: NavBarData[] = [];
+</script>
+
+<nav>
+	{#each data as item}
+		<a href={item.link}>
+			{#if item.title}
+				{item.title}
+			{/if}
+
+			<Icon name={item.icon} /></a>
+	{/each}
+</nav>
+
+<style lang="scss">
+	@use '@surveyplanet/styles' as *;
+	nav {
+		display: flex;
+		max-width: fit-content;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	a {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+		padding: $size--18;
+	}
+</style>
---
+++ src/stories/navbar/NavBar.story.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { logEvent } from 'histoire/client';
+	import type { Hst as Histoire } from '@histoire/plugin-svelte';
+	import { NavBar, Button } from '../../lib';
+	import { json } from '@sveltejs/kit';
+	export let Hst: Histoire;
+
+	let data = [
+		{
+			icon: 'edit',
+
+			link: '#',
+		},
+		{
+			icon: 'chartColumn',
+
+			link: '#',
+		},
+		{
+			icon: 'share',
+
+			link: '#',
+		},
+		{
+			icon: 'ellipses',
+
+			link: '#',
+		},
+	];
+</script>
+
+<Hst.Story title="NavBar">
+	<svelte:fragment slot="controls">
+		<Hst.Json
+			bind:value={data}
+			title="Data" />
+	</svelte:fragment>
+
+	<Hst.Variant title="NavBar">
+		<NavBar {data} />
+	</Hst.Variant>
+</Hst.Story>
+
+<style>
+	.button-for-tests {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
