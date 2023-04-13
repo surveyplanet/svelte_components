7887ba5cd18fd549ebec3344a2dd64241c3a19b4
serferdinand2 <ognjen@surveyplanet.com>
2023-04-13 22:42:57 +0200

    checkin footer component

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,43 @@
+<script
+	lang="ts"
+	context="module">
+	export interface FooterData {
+		link?: string;
+		label: string;
+	}
+</script>
+
+<script lang="ts">
+	export let footerData: FooterData[] = [];
+</script>
+
+<nav class="sp-footer">
+	{#each footerData as item}
+		<a
+			class="sp-footer--item"
+			href={item.link}>{item.label}</a>
+		{#if footerData.indexOf(item) < footerData.length - 1}
+			<span class="sp-footer--divider"> · </span>
+		{/if}
+	{/each}
+</nav>
+
+<style lang="scss">
+	@use '@surveyplanet/styles' as *;
+	.sp-footer {
+		font-family: $font--default;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 1rem;
+		a {
+			text-decoration: none;
+			padding: 0.5rem;
+			color: $color--purple;
+		}
+		.sp-footer--divider {
+			color: $color--purple;
+			padding: 0.5rem;
+		}
+	}
+</style>
---
+++ src/stories/footer/Footer.story.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import type { Hst as Histoire } from '@histoire/plugin-svelte';
+	import { Footer, type FooterData } from '../../lib';
+	import { logEvent } from 'histoire/client';
+
+	// import { default as source } from './source';
+	export let Hst: Histoire;
+
+	let footerData: FooterData[] = [
+		{
+			link: 'https://www.surveyplanet.com/about',
+			label: 'About',
+		},
+		{
+			link: 'https://www.surveyplanet.com/terms',
+			label: 'Terms Of Service',
+		},
+		{
+			link: 'https://www.surveyplanet.com/contact',
+			label: 'Contact',
+		},
+		{
+			label: '© 2022 SurveyPlanet, LLC',
+		},
+	];
+</script>
+
+<Hst.Story title="Footer">
+	<svelte:fragment slot="controls">
+		<Hst.Json
+			bind:value={footerData}
+			title="Footer items" />
+	</svelte:fragment>
+
+	<Hst.Variant title="Primary">
+		<div class="wrapper">
+			<Footer {footerData} />
+		</div>
+	</Hst.Variant>
+</Hst.Story>
