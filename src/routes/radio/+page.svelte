acc735b80ae86f2880fa0363f70e963d6dd0b970
Jonah Werre <jonahwerre@gmail.com>
2024-02-22 14:12:50 -0700

    refactored radio, checkbox and toggle

--- src/routes/radio/+page.svelte
+++ src/routes/radio/+page.svelte
@@ -4,88 +4,74 @@
 	import { Layout, PropsChanger } from '$layout/index';
 	import md from './docs.md?raw';
 	import { default as source } from './example';
-	let events = $state<ComponentEvent<boolean>[]>([]);
+	let events = $state<ComponentEvent<string>[]>([]);
+	let data: RadioProps['data'] = $state([
+		{
+			id: 'harry-radio',
+			label: 'Harry',
+			value: 'harry',
+		},
+		{
+			id: 'larry-radio',
+			label: 'Larry',
+			value: 'larry',
+		},
+		{
+			id: 'moe-radio',
+			label: 'Moe',
+			value: 'moe',
+		},
+	]);
 
-	let id: RadioProps['id'] = 'radio';
-	let name: RadioProps['name'] = $state('stooge');
-	let label: RadioProps['label'] = $state('Harry');
-	let labelTwo: RadioProps['label'] = $state('Larry');
-	let labelThree: RadioProps['label'] = $state('Moe');
-	let disabled: RadioProps['disabled'] = $state(false);
-	let prependLabel: RadioProps['prependLabel'] = $state(false);
+	let group: RadioProps['group'] = $state();
+	let prependLabel: RadioProps['prependLabel'] = $state();
+	let block: RadioProps['block'] = $state();
 	let size: RadioProps['size'] = $state('small');
 
-	const onRadioChange = (event: ComponentEvent<boolean>): void => {
+	const onRadioChange = (event: ComponentEvent<string>): void => {
 		events.push(event);
 	};
 </script>
 
 <Layout
 	component="Radio"
-	example={source({ id, name, label, disabled, prependLabel, size })}
+	example={source({ group, data, prependLabel, size })}
 	{md}
 	bind:events>
 	{#snippet controls()}
 		<PropsChanger
-			label="Name"
-			bind:value={name} />
+			label="Group"
+			type="string"
+			bind:value={group} />
 		<PropsChanger
-			label="Label One"
-			bind:value={label} />
-		<PropsChanger
-			label="Label Two"
-			bind:value={labelTwo} />
-		<PropsChanger
-			label="Label Three"
-			bind:value={labelThree} />
-		<PropsChanger
-			label="Disabled"
-			bind:value={disabled} />
-		<PropsChanger
-			label="Prepend Label"
-			bind:value={prependLabel} />
+			label="Data"
+			bind:value={data} />
 		<PropsChanger
 			label="Size"
 			selectOptions={['small', 'medium', 'large']}
 			bind:value={size} />
+		<PropsChanger
+			label="Prepend Label"
+			type="boolean"
+			bind:value={prependLabel} />
+		<PropsChanger
+			label="Block"
+			type="boolean"
+			bind:value={block} />
 	{/snippet}
+
 	{#snippet main()}
 		<div id="wrapper">
 			<Radio
-				id={label!.toLowerCase().replace(' ', '-')}
-				{disabled}
-				{name}
-				value={label}
-				{label}
+				{data}
 				{prependLabel}
 				{size}
-				{onRadioChange} />
-			<Radio
-				id={labelTwo!.toLowerCase().replace(' ', '-')}
-				{disabled}
-				{name}
-				value={labelTwo}
-				label={labelTwo}
-				{prependLabel}
-				{size}
-				{onRadioChange} />
-			<Radio
-				id={labelThree!.toLowerCase().replace(' ', '-')}
-				{disabled}
-				{name}
-				value={labelThree}
-				label={labelThree}
-				{prependLabel}
-				{size}
+				{block}
+				bind:group
 				{onRadioChange} />
 		</div>
 	{/snippet}
 </Layout>
 
-<style>
-	#wrapper {
-		display: flex;
-		gap: 24px;
-		padding: 24px;
-	}
+<style lang="scss">
 </style>
